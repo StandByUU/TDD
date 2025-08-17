e78b31a547f3996ff32f64dbc419e7207753ef7a
Vladimir <willfordrockwell@gmail.com>
2025-08-17 23:10:12 +0800

    Update PhoneBook.java
    Stub and tests for add

--- TDD/src/main/java/PhoneBook.java
+++ TDD/src/main/java/PhoneBook.java
@@ -1,5 +1,21 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class PhoneBook {
+    private final Map<String, String> nameToNumber;
+    private final Map<String, String> numberToName;
+
+    public PhoneBook() {
+        nameToNumber = new HashMap<>();
+        numberToName = new HashMap<>();
+    }
+
     public int add(String name, String number) {
-        return 0;
+        if (nameToNumber.containsKey(name)) {
+            return nameToNumber.size(); // не добавляем, возвращаем текущее кол-во
+        }
+        nameToNumber.put(name, number);
+        numberToName.put(number, name);
+        return nameToNumber.size();
     }
 }
