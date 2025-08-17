95162e68ba8ab40c70f33c6c6abf79118d09575c
Vladimir <willfordrockwell@gmail.com>
2025-08-17 23:20:53 +0800

    Update PhoneBook.java

--- TDD/src/main/java/PhoneBook.java
+++ TDD/src/main/java/PhoneBook.java
@@ -1,26 +1,32 @@
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class PhoneBook {
     private final Map<String, String> nameToNumber;
     private final Map<String, String> numberToName;
 
     public PhoneBook() {
-        nameToNumber = new HashMap<>();
+        nameToNumber = new TreeMap<>();
         numberToName = new HashMap<>();
     }
 
     public int add(String name, String number) {
         if (nameToNumber.containsKey(name)) {
-            return nameToNumber.size(); // не добавляем, возвращаем текущее кол-во
+            return nameToNumber.size();
         }
         nameToNumber.put(name, number);
         numberToName.put(number, name);
         return nameToNumber.size();
     }
-}
 
-public String findByNumber(String number) {
-    return null;
-}
+    public String findByNumber(String number) {
+        return numberToName.get(number);
+    }
 
+    public String findByName(String name) {
+        return nameToNumber.get(name);
+    }
+
+    public void printAllNames() {
+        nameToNumber.keySet().forEach(System.out::println);
+    }
+}
